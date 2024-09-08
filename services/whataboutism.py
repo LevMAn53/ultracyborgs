64a42ce665e921e0535fdd1abb9d0afa839a477a
yehorkhod <e.khodysko@gmail.com>
2024-09-08 09:27:57 +0300

    feat: update whataboutism

--- services/whataboutism.py
+++ services/whataboutism.py
@@ -2,21 +2,27 @@ import re
 
 ID: int = 1
 
+WAT_PATTERNS: dict[str, str] = {
+    "what about...?" : r'\bwhat about\b.*\?',
+    "as for..." : r'\bas for\b.*\ ',
+    "how about...?" : r'\bhow about\b.*\?',
+    "why don’t (somebody) mention...?" : r'\bwhy (don’t|didn’t|won’t) ((\w+)|(\w+)\s(\w+)) mention\b.*\?',
+    "but (somebody) never mention..." : r'\bbut ((\w+)|(\w+)\s(\w+)) never mention\b.* ',
+    "what do (somebody) have to say about...?" : r'\bwhat (do|does) ((\w+)|(\w+)\s(\w+)) (have|has) to say about\b.*\?',
+    "why are (somebody) not talking about...?" : r'\bwhy (are|is) ((\w+)|(\w+)\s(\w+)) not talking about\b.*\?',
+    "how can (somebody) ignore...?" : r'\bhow can ((\w+)|(\w+)\s(\w+)) ignore\b.*\?',
+
+}
+
 # Function to detect the phrase "What about...?" and return a warning
 def whataboutism_service(tweet: str) -> list | None:
     sentences = re.split(r'(?<=[.!?]) +', tweet)
 
-    # Define the pattern for "What about...?"
-    pattern1 = r'\bwhat about\b.*\?'
-    pattern2 = r'\bas for\b.*\ '
-    
     whataboutism_sentances = []
 
     for sentance in sentences:
-        match1 = re.search(pattern1, sentance, re.IGNORECASE)
-        match2 = re.search(pattern2, sentance, re.IGNORECASE)
-
-        if match1 or match2:
-            whataboutism_sentances.append((ID, sentance, None))
+        for name, pattern in WAT_PATTERNS.items():
+            if re.search(pattern, sentance, re.IGNORECASE):
+                whataboutism_sentances.append(sentance)
 
     return whataboutism_sentances if len(whataboutism_sentances) > 0 else None
